cd4d9cb5cb73798cc41bb19b0d6494e01169bb80
Fong NGO <fong.ngo.fn@gmail.com>
2019-07-12 22:45:36 +0200

    handle post requests

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import (
+    Flask, jsonify, request
+)
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/tasks', methods=['POST'])
+def assign_tasks():
+    #if (
+    #    request.method != 'POST' or
+    #    request.headers['content-type'] != 'application/json'
+    #):
+    #    return jsonify(['NOK']), 405
+
+    tasks = request.data
+    return tasks
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
