0d4889a62cca7f84382800fdff6e96fb19e8bb4c
Fong NGO <fong.ngo.fn@gmail.com>
2019-07-13 03:52:18 +0200

    submit assigned tasks to airhack api

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import (
     Flask, jsonify, request
 )
+import requests
+
+from assign_tasks import find_assignees
 
 app = Flask(__name__)
 
+TOKEN = 'bUhC8AMDCOjkrsz0a453g9gZdq3TSxpglFH0JeEgUDw5EUWMhIEZNekZ4faC'
 
 @app.route('/tasks', methods=['POST'])
 def assign_tasks():
@@ -14,7 +18,17 @@ def assign_tasks():
     #    return jsonify(['NOK']), 405
 
     tasks = request.data
-    return tasks
+    tasks_with_assignees = find_assignees(tasks)
+
+    submit_url = 'http://airhack-api.herokuapp.com/api/submitTasks'
+    response = requests.post(
+        submit_url,
+        headers={'Authorization': 'Bearer ' + TOKEN},
+        json=tasks_with_assignees,
+    )
+
+    print(response.json())
+    return jsonify(tasks_with_assignees)
 
 
 if __name__ == '__main__':
